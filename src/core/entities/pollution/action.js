54640945dea3daadfb4127a675576d0b38b4d7d0
jinu <jinu@promasoft.net>
2021-08-15 18:00:02 +0530

    Redux set-up

---
+++ src/core/entities/pollution/action.js
@@ -0,0 +1,21 @@
+import { ActionType } from "./actionType";
+
+export const PollutionAction = {
+	getCountry() {
+		return {
+			type: ActionType.FETCH_COUNTRY_BEGIN,
+		};
+	},
+	getCity(country) {
+		return {
+			type: ActionType.FETCH_CITY_BEGIN,
+			country,
+		};
+	},
+	getMeasurement(values) {
+		return {
+			type: ActionType.FETCH_MEASUREMENT_BEGIN,
+			values,
+		};
+	},
+};
