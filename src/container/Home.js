276aa4de2074d706cc33fc2de500dceb8d535fdd
jinu <jinu@promasoft.net>
2021-08-15 20:38:19 +0530

    Home page, Navbar

---
+++ src/container/Home.js
@@ -0,0 +1,161 @@
+import { Formik } from "formik";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Bar } from "react-chartjs-2";
+
+import Card from "../component/UI/Card/Card";
+import CardBody from "../component/UI/Card/CardBody";
+import { PollutionAction } from "../core/entities/pollution/action";
+import groupDataItem from "../utils/groupDataItem";
+import BarChart from "../component/UI/Chart/BarChart";
+import TextInput from "../component/Form/TextInput";
+
+const Home = () => {
+	const dispatch = useDispatch();
+
+	const [parameters, setParameters] = useState([]);
+	const [pollutionData, setPollutionData] = useState([]);
+	const [pollutionValue, setPollutionValue] = useState([]);
+	const [pollutionDate, setPollutionDate] = useState([]);
+
+	const countries = useSelector((state) => state.pollution.country.results);
+	const cities = useSelector((state) => state.pollution.city.results);
+	const measurements = useSelector(
+		(state) => state.pollution.measurement.results,
+	);
+
+	useEffect(() => {
+		dispatch(PollutionAction.getCountry());
+	}, []);
+
+	useEffect(() => {
+		const unique = [...new Set(measurements.map((item) => item.parameter))];
+		setParameters(unique);
+	}, [dispatch, measurements]);
+
+	const fetchCities = (e) => {
+		dispatch(PollutionAction.getCity(e.target.value));
+	};
+
+	const handleSubmit = (values, { setSubmitting }) => {
+		console.log(values);
+		dispatch(PollutionAction.getMeasurement(values));
+	};
+
+	const groupData = async (e) => {
+		let dates = [],
+			values = [];
+		const parameterName = e.target.innerText;
+		var groupedData = groupDataItem(measurements, "parameter");
+		await setPollutionData(groupedData[parameterName]);
+
+		for (const pollutionDataItem of pollutionData) {
+			dates.push(pollutionDataItem.date.local);
+			values.push(pollutionDataItem.value);
+		}
+
+		setPollutionDate(dates);
+		setPollutionValue(values);
+	};
+
+	return (
+		<div>
+			<Card>
+				<CardBody>
+					<Formik
+						initialValues={{
+							country: "",
+							city: "",
+							date_from: "",
+							date_to: "",
+						}}
+						onSubmit={(values, { setSubmitting }) => {
+							handleSubmit(values, { setSubmitting });
+						}}>
+						{({
+							values,
+							errors,
+							touched,
+							handleChange,
+							handleBlur,
+							handleSubmit,
+							isSubmitting,
+						}) => (
+							<form onSubmit={handleSubmit}>
+								<div class='row'>
+									<div class='col'>
+										<select
+											class='form-select'
+											name={"country"}
+											value={values.country}
+											onBlur={handleBlur}
+											onChange={(e) => {
+												fetchCities(e);
+												handleChange(e);
+											}}>
+											<option selected>Country</option>
+											{countries.map((d) => (
+												<option value={d.code}>{d.name}</option>
+											))}
+										</select>
+									</div>
+									<div class='col'>
+										<select
+											class='form-select'
+											name={"city"}
+											value={values.city}
+											onChange={handleChange}
+											onBlur={handleBlur}>
+											<option selected>City</option>
+											{cities.length > 0
+												? cities.map((d) => (
+														<option value={d.city}>{d.city}</option>
+												  ))
+												: null}
+										</select>
+									</div>
+									<div class='col'>
+										<TextInput
+											type='date'
+											name='date_from'
+											value={values.date_from}
+											onChange={handleChange}
+											onBlur={handleBlur}
+										/>
+									</div>
+									<div class='col'>
+										<TextInput
+											type='date'
+											name='date_to'
+											value={values.date_to}
+											onChange={handleChange}
+											onBlur={handleBlur}
+										/>
+									</div>
+									<div class='col'>
+										<button className='btn btn-primary' type='submit'>
+											GO
+										</button>
+									</div>
+								</div>
+							</form>
+						)}
+					</Formik>
+				</CardBody>
+			</Card>
+			<div class='d-flex flex-row bd-highlight mb-3 mt-3'>
+				{parameters.map((p) => (
+					<button class='btn btn-success btn-sm m-2' onClick={groupData}>
+						{p}
+					</button>
+				))}
+			</div>
+
+			{pollutionDate.length > 0 && pollutionValue.length > 0 && (
+				<BarChart labels={pollutionDate} datasets={pollutionValue} />
+			)}
+		</div>
+	);
+};
+
+export default Home;
---
+++ src/layout/NavBar/NavBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const NavBar = () => {
+	return (
+		<nav className='navbar navbar-expand-lg navbar-dark bg-primary mb-3'>
+			<div className='container-fluid'>
+				<Link to={"/"} className='navbar-brand'>
+					Pollution Data
+				</Link>
+				<button
+					className='navbar-toggler'
+					type='button'
+					data-bs-toggle='collapse'
+					data-bs-target='#navbarSupportedContent'
+					aria-controls='navbarSupportedContent'
+					aria-expanded='false'
+					aria-label='Toggle navigation'>
+					<span className='navbar-toggler-icon'></span>
+				</button>
+				<div className='collapse navbar-collapse' id='navbarSupportedContent'>
+					<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+						<li className='nav-item'>
+							<NavLink to={"/"} className='nav-link'>
+								Home
+							</NavLink>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default NavBar;
