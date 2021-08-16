bd2f965ac861570769d004f1bba4d3445d96ca62
jinu <jinu@promasoft.net>
2021-08-16 12:39:01 +0530

    working fine

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,7 +1,6 @@
-import { Formik } from "formik";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Bar } from "react-chartjs-2";
+import { Formik, ErrorMessage } from "formik";
 
 import Card from "../component/UI/Card/Card";
 import CardBody from "../component/UI/Card/CardBody";
@@ -9,20 +8,22 @@ import { PollutionAction } from "../core/entities/pollution/action";
 import groupDataItem from "../utils/groupDataItem";
 import BarChart from "../component/UI/Chart/BarChart";
 import TextInput from "../component/Form/TextInput";
+import TextError from "./TextError";
+import * as Yup from "yup";
 
 const Home = () => {
 	const dispatch = useDispatch();
-
-	const [parameters, setParameters] = useState([]);
-	const [pollutionData, setPollutionData] = useState([]);
-	const [pollutionValue, setPollutionValue] = useState([]);
-	const [pollutionDate, setPollutionDate] = useState([]);
 
 	const countries = useSelector((state) => state.pollution.country.results);
 	const cities = useSelector((state) => state.pollution.city.results);
 	const measurements = useSelector(
 		(state) => state.pollution.measurement.results,
 	);
+
+	const [parameters, setParameters] = useState([]);
+	const [pollutionData, setPollutionData] = useState([]);
+	const [pollutionValue, setPollutionValue] = useState([]);
+	const [pollutionDate, setPollutionDate] = useState([]);
 
 	useEffect(() => {
 		dispatch(PollutionAction.getCountry());
@@ -37,8 +38,7 @@ const Home = () => {
 		dispatch(PollutionAction.getCity(e.target.value));
 	};
 
-	const handleSubmit = (values, { setSubmitting }) => {
-		console.log(values);
+	const handleSubmit = (values) => {
 		dispatch(PollutionAction.getMeasurement(values));
 	};
 
@@ -58,17 +58,27 @@ const Home = () => {
 		setPollutionValue(values);
 	};
 
+	const initialValues = {
+		country: "",
+		city: "",
+		date_from: "",
+		date_to: "",
+	};
+
+	const validationSchema = Yup.object({
+		country: Yup.string().required("Choose a country"),
+		city: Yup.string().required("Choose a city"),
+		date_from: Yup.date().required("Choose a From date"),
+		date_to: Yup.date().required("Choose a To date"),
+	});
+
 	return (
 		<div>
 			<Card>
 				<CardBody>
 					<Formik
-						initialValues={{
-							country: "",
-							city: "",
-							date_from: "",
-							date_to: "",
-						}}
+						initialValues={initialValues}
+						validationSchema={validationSchema}
 						onSubmit={(values, { setSubmitting }) => {
 							handleSubmit(values, { setSubmitting });
 						}}>
@@ -98,6 +108,11 @@ const Home = () => {
 												<option value={d.code}>{d.name}</option>
 											))}
 										</select>
+										<ErrorMessage
+											className='error-message'
+											name='country'
+											component={TextError}
+										/>
 									</div>
 									<div class='col'>
 										<select
@@ -113,7 +128,14 @@ const Home = () => {
 												  ))
 												: null}
 										</select>
+										<ErrorMessage
+											className='error-message'
+											name='city'
+											component={TextError}
+										/>
 									</div>
+								</div>
+								<div className='row' style={{ marginTop: "15px" }}>
 									<div class='col'>
 										<TextInput
 											type='date'
@@ -121,6 +143,11 @@ const Home = () => {
 											value={values.date_from}
 											onChange={handleChange}
 											onBlur={handleBlur}
+										/>
+										<ErrorMessage
+											className='error-message'
+											name='date_from'
+											component={TextError}
 										/>
 									</div>
 									<div class='col'>
@@ -131,10 +158,23 @@ const Home = () => {
 											onChange={handleChange}
 											onBlur={handleBlur}
 										/>
+										<ErrorMessage
+											className='error-message'
+											name='date_to'
+											component={TextError}
+										/>
 									</div>
+								</div>
+								<div className='row' style={{ marginTop: "15px" }}>
 									<div class='col'>
-										<button className='btn btn-primary' type='submit'>
+										<button
+											className='btn btn-primary'
+											type='submit'
+											style={{ backgroundColor: "#232f3b" }}>
 											GO
+											<i
+												className='fa fa-long-arrow-right'
+												style={{ paddingLeft: "10px" }}></i>
 										</button>
 									</div>
 								</div>
@@ -145,7 +185,10 @@ const Home = () => {
 			</Card>
 			<div class='d-flex flex-row bd-highlight mb-3 mt-3'>
 				{parameters.map((p) => (
-					<button class='btn btn-success btn-sm m-2' onClick={groupData}>
+					<button
+						class='btn btn-success btn-sm m-2'
+						style={{ backgroundColor: "#232f3b" }}
+						onClick={groupData}>
 						{p}
 					</button>
 				))}
--- src/layout/NavBar/NavBar.js
+++ src/layout/NavBar/NavBar.js
@@ -1,32 +1,14 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import "./navbar.css";
 
 const NavBar = () => {
 	return (
-		<nav className='navbar navbar-expand-lg navbar-dark bg-primary mb-3'>
+		<nav className='navbar navbar-expand-lg navbar-dark bg-primary mb-3 navbarBG'>
 			<div className='container-fluid'>
 				<Link to={"/"} className='navbar-brand'>
-					Pollution Data
+					OpenAQ - Pollution Data
 				</Link>
-				<button
-					className='navbar-toggler'
-					type='button'
-					data-bs-toggle='collapse'
-					data-bs-target='#navbarSupportedContent'
-					aria-controls='navbarSupportedContent'
-					aria-expanded='false'
-					aria-label='Toggle navigation'>
-					<span className='navbar-toggler-icon'></span>
-				</button>
-				<div className='collapse navbar-collapse' id='navbarSupportedContent'>
-					<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-						<li className='nav-item'>
-							<NavLink to={"/"} className='nav-link'>
-								Home
-							</NavLink>
-						</li>
-					</ul>
-				</div>
 			</div>
 		</nav>
 	);
